1532447c45424e5053cd3daf37c8b1384e24f79d
haochen21 <chenhao21@163.com>
2019-05-23 11:11:12 +0800

    init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,10 +12,20 @@ import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 
 import { ChartModule } from 'angular2-highcharts';
+import * as highcharts from 'highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 
 registerLocaleData(zh);
 
 declare var require: any;
+
+export function highchartsFactory() {
+  const hc = require('highcharts');
+  const dd = require('highcharts/modules/drilldown');
+  dd(hc);
+
+  return hc;
+}
 
 @NgModule({
   declarations: [
@@ -29,9 +39,17 @@ declare var require: any;
     HttpClientModule,
     CommonModule, 
     BrowserAnimationsModule,
-    ChartModule.forRoot(require('highcharts'))
+    ChartModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [
+    {
+      provide: NZ_I18N,
+      useValue: en_US
+    },
+    {
+      provide: HighchartsStatic,
+      useFactory: highchartsFactory
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
